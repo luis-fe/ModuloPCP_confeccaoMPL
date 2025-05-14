27eedf5b388a64c55836f9fbdee773c43e9ed5e4
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-05-14 16:06:40 -0300

    implantando a simulacao detalhada por sku

--- src/models/Pedidos.py
+++ src/models/Pedidos.py
@@ -107,6 +107,7 @@ class Pedidos():
         #4 Filtrando de acordo com os intervalos encontrados de Vendas e Faturamento
         df_loaded['dataEmissao'] = pd.to_datetime(df_loaded['dataEmissao'], errors='coerce', infer_datetime_format=True)
         df_loaded['dataPrevFat'] = pd.to_datetime(df_loaded['dataPrevFat'], errors='coerce', infer_datetime_format=True)
+        print(self.iniVendas)
         df_loaded['filtro'] = df_loaded['dataEmissao'] >= self.iniVendas
         df_loaded['filtro2'] = df_loaded['dataEmissao'] <= self.fimVendas
         df_loaded['filtro3'] = df_loaded['dataPrevFat'] >= self.iniFat
