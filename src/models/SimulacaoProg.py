ff23314db91a1086a00dbef46bb1d084a36c6ecf
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-05-16 09:44:34 -0300

    modificando a api de simulacao de tendencia no backEnd

--- src/models/SimulacaoProg.py
+++ src/models/SimulacaoProg.py
@@ -322,14 +322,12 @@ class SimulacaoProg():
         consultaAbcSimulacao = pd.read_sql(consultaAbcSimulacao,conn , params=(self.nomeSimulacao,))
 
         consultaSimulacao = pd.merge(consultaParamentrosAbc, consultaAbcSimulacao , on='class', how='left')
-        consultaSimulacao['percentual'] = consultaSimulacao['percentual'].astype(str)
-        consultaSimulacao['percentual'].fillna('-', inplace=True)
+        consultaSimulacao['percentual'].fillna(0, inplace=True)
 
         consultaCategoriaSimulacao = pd.read_sql(consultaCategoriaSimulacao,conn , params=(self.nomeSimulacao,))
         consultaCategoria = pd.read_sql(consultaCategoria, conn)
         consultaCategoria = pd.merge(consultaCategoria,consultaCategoriaSimulacao,on='categoria', how='left' )
-        consultaSimulacao['percentual'] = consultaSimulacao['percentual'].astype(str)
-        consultaSimulacao['percentual'].fillna('-', inplace=True)
+        consultaSimulacao['percentual'].fillna(100, inplace=True)
 
         consultaMarcaSimulacao = pd.read_sql(consultaMarcaSimulacao,conn , params=(self.nomeSimulacao,))
         consultaMarcas = pd.read_sql(consultaMarcas, conn)
