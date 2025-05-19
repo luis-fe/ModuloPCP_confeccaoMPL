122068e01b976908a1f3a0df697aea545249d43e
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-05-19 19:07:03 -0300

    calculo da necessidade em peca baseado na materia prima, corrigindo para carregar csv

--- src/models/Tendencia_Plano_Materiais.py
+++ src/models/Tendencia_Plano_Materiais.py
@@ -379,7 +379,7 @@ class Tendencia_Plano_Materiais():
         sqlEstoque = sqlEstoque.groupby(["CodComponente"]).agg(
             {"estoqueAtual": "sum"}).reset_index()
 
-        Necessidade['CodComponente'] = Necessidade['CodComponente'].astype(str)
+        Necessidade['CodComponente'] = Necessidade['CodComponente'].astype(float).astype(int).astype(str)
         Necessidade = pd.merge(Necessidade, sqlEstoque, on='CodComponente', how='left')
 
         Necessidade.to_csv(f'{caminho_absoluto2}/dados/MeuTeste2.csv')
