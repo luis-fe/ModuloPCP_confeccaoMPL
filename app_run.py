30221020e04ba36e5fe5c8a54ca19776eb1df292
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-06-13 15:52:59 -0300

    api para obter a imagem de um item pai do color book

--- app_run.py
+++ app_run.py
@@ -4,8 +4,12 @@ import os
 from src.configApp import configApp
 from src.routes import routes_blueprint  # Certifique-se de que 'routes.py' está na mesma pasta
 from dotenv import load_dotenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
+
 env_path = configApp.localProjeto
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv(f'{env_path}/_ambiente.env')
