971935b62a9490dc464d76db8d1495bfd914694d
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-08-04 08:36:02 -0300

    testando o serviço de automacao

---
+++ src/models/Componentes_Csw.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import fastparquet as fp
+from src.connection import ConexaoERP
+
+
+
+class Componentes_CSW():
+
+    def __init__(self, codEmpresa = '1', intervalo_automacao = 100):
+
+        self.codEmpresa = codEmpresa
+        self.intervalo_automacao = intervalo_automacao # Atrubuto para Controlar o intervalo de automacao em Segundos
+    def inserirComponentesVariaveis(self):
+
+        sql = """
+        SELECT 
+                            v.codProduto as codEngenharia, 
+                            cv.codSortimento, 
+                            cv.seqTamanho as codSeqTamanho,  
+                            v.CodComponente,
+                            (SELECT i.nome FROM cgi.Item i WHERE i.codigo = v.CodComponente) as descricaoComponente,
+                            (SELECT i.unidadeMedida FROM cgi.Item i WHERE i.codigo = v.CodComponente) as unid,
+                            cv.quantidade  
+                        from 
+                            tcp.ComponentesVariaveis v 
+                        join 
+                            tcp.CompVarSorGraTam cv 
+                            on cv.codEmpresa = v.codEmpresa 
+                            and cv.codProduto = v.codProduto 
+                            and cv.sequencia = v.codSequencia 
+                        WHERE 
+                            v.codEmpresa = 1
+                            and v.codClassifComponente <> 12
+                        UNION 
+                        SELECT 
+                            v.codProduto as codEngenharia,  
+                            l.codSortimento ,
+                            l.codSeqTamanho  as codSeqTamanho, 
+                            v.CodComponente,
+                            (SELECT i.nome FROM cgi.Item i WHERE  i.codigo = v.CodComponente) as descricaoComponente,
+                            (SELECT i.unidadeMedida FROM cgi.Item i WHERE i.codigo = v.CodComponente) as unid,
+                            v.quantidade  
+                        from 
+                            tcp.ComponentesPadroes  v 
+                        join 
+                            cgi.Item2 l
+                            on l.Empresa  = v.codEmpresa
+                            and v.codEmpresa = 1
+                            and l.codCor  > 0
+                            and '0'||l.codItemPai ||'-0' = v.codProduto
+                        where
+                            l.Empresa  = 1
+        """
+        with ConexaoERP.ConexaoInternoMPL() as conn:
+            with conn.cursor() as cursor_csw:
+                # Executa a primeira consulta e armazena os resultados
+                cursor_csw.execute(sql)
+                colunas = [desc[0] for desc in cursor_csw.description]
+                rows = cursor_csw.fetchall()
+                compVar = pd.DataFrame(rows, columns=colunas)
+                del rows, colunas
+
+        fp.write('./dados/compVar.parquet', compVar)
+
+        return compVar
