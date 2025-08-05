82952899049b16b154c3dbe64854a8e628da6c54
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-08-05 10:20:28 -0300

    adicionando o monitor pedidos no novo srv

--- src/models/Pedidos.py
+++ src/models/Pedidos.py
@@ -144,7 +144,12 @@ class Pedidos():
             print(f'Erro ao converter dataEmissao. iniVendas: {self.iniVendas} | Erro:\n {self.iniVendas} ')
 
         df_loaded['dataPrevFat'] = pd.to_datetime(df_loaded['dataPrevFat'], errors='coerce', infer_datetime_format=True)
-        df_loaded['filtro'] = df_loaded['dataEmissao'] >= self.iniVendas
+
+        try:
+         df_loaded['filtro'] = df_loaded['dataEmissao'] >= self.iniVendas
+        except Exception as e:
+            print(f'Erro ao converter dataEmissao. iniVendas: {self.iniVendas} | Erro{e}:\n {self.iniVendas} ')
+
         df_loaded['filtro2'] = df_loaded['dataEmissao'] <= self.fimVendas
         df_loaded['filtro3'] = df_loaded['dataPrevFat'] >= self.iniFat
         df_loaded['filtro4'] = df_loaded['dataPrevFat'] <= self.fimFat
