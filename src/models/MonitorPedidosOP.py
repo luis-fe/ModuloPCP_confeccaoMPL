69ec29be28c5bee8020a5f5f2c3d0f33a2ef77f6
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-08-05 12:29:19 -0300

    mudancas na estruturacao

--- src/models/MonitorPedidosOP.py
+++ src/models/MonitorPedidosOP.py
@@ -34,7 +34,7 @@ class MonitorPedidosOP():
         self.filtroDataEmissaoIni = filtroDataEmissaoIni
         self.filtroDataEmissaoFim = filtroDataEmissaoFim
         self.analiseOPGarantia = analiseOPGarantia
-        self.descricaoArquivo = f'{self.empresa}_{self.dataInicioFat}_{self.dataFinalFat}'
+        self.descricaoArquivo = f'empresa_{self.empresa}_prevfat_{self.dataInicioFat}_{self.dataFinalFat}'
         self.arrayTipoNota = '1,2,3,4,5,6,7,8,10,24,92,201,1012,77,27,28,172,9998,66,67,233,237'  # Arrumar o Tipo de Nota 40
         self.arrayOPSimuladas = arrayOPSimuladas
 
