eccec7f57ffe3baee7e3323cbff1abb27405b82a
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-08-05 15:08:22 -0300

    mudancas na estruturacao

--- src/models/MonitorPedidosOP.py
+++ src/models/MonitorPedidosOP.py
@@ -649,11 +649,11 @@ class MonitorPedidosOP():
         load_dotenv(f'{env_path}/_ambiente.env')
         caminhoAbsoluto = os.getenv('CAMINHO_PARQUET_FAT')
 
-        try:
-            fp.write(f'{caminhoAbsoluto}/monitor{self.descricaoArquivo}.parquet', pedidos)
-        except:
-            print(pedidos.columns.tolist())
-            pedidos.to_csv(f'{self.descricaoArquivo}_erro.csv')
+        #try:
+        fp.write(f'{caminhoAbsoluto}/monitor{self.descricaoArquivo}.parquet', pedidos)
+        #except:
+            #print(pedidos.columns.tolist())
+            #pedidos.to_csv(f'{self.descricaoArquivo}_erro.csv')
 
         # etapa25 = controle.salvarStatus_Etapa25(rotina, ip, etapa24, 'Salvando os dados gerados no postgre')#Registrar etapa no controlador
         return pedidos
