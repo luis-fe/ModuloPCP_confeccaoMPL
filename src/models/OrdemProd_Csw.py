23da5869141964634b3c394031700cef0d84bfbf
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-10-20 14:38:34 -0300

    atualizando a melhoria do dashboard 2 Qualidade

--- src/models/OrdemProd_Csw.py
+++ src/models/OrdemProd_Csw.py
@@ -137,8 +137,8 @@ class OrdemProd_Csw():
 							WHERE 
 								mf.codempresa = 1 
 								and mf.codfase = 429
-								and mf.databaixa >= '{datainicial}'
-								and mf.databaixa <= '{datafinal}'
+								and mf.databaixa >= DATEADD('{datainicial}', -40, CURRENT_TIMESTAMP)
+								and mf.databaixa <= DATEADD('{datafinal}', +40, CURRENT_TIMESTAMP)
                         )  
                         and tiprem = 1 
                         and r.codfase = 429
