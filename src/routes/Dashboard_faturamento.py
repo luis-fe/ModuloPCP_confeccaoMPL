ecdb7589196fc04d3ccb05f10383793b278c6713
luis.fernando <luiis_engp@hotmail.com>
2026-01-07 12:12:33 -0300

    atualizando as metas

--- src/routes/Dashboard_faturamento.py
+++ src/routes/Dashboard_faturamento.py
@@ -102,3 +102,24 @@ def get_usuarios():
         OP_data.append(op_dict)
     del dados
     return jsonify(OP_data)
+
+
+@dashboard_fat_routes.route('/pcp/api/autentificarUsuarioSenhaMetas', methods=['GET'])
+@token_required
+def get_autentificarUsuarioSenhaMetas():
+    matricula = request.args.get('matricula')
+    senha = request.args.get('senha')
+
+    dados = DashboardTV.DashboardTV('','','','',matricula, senha).usuario_autentificar()
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    del dados
+    return jsonify(OP_data)
