892174741f02a2e973b2db58bcb42e7028fcc7e9
luis.fernando <luiis_engp@hotmail.com>
2026-01-09 09:49:10 -0300

    atualizando informacao relativa a ultima alteracao de meta realizada

--- src/routes/Dashboard_faturamento.py
+++ src/routes/Dashboard_faturamento.py
@@ -153,3 +153,26 @@ def post_salvarUsuarioSenha():
         OP_data.append(op_dict)
     del dados
     return jsonify(OP_data)
+
+
+
+@dashboard_fat_routes.route('/pcp/api/get_informacaoUltimaAlteMetas', methods=['GET'])
+@token_required
+def get_informacaoUltimaAlteMetas():
+    codEmpresa = request.args.get('Empresa','1')
+    ano =  request.args.get('ano','1')
+
+
+    dados = DashboardTV.DashboardTV(codEmpresa, ano,'','').get_ultima_alteracao()
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    del dados
+    return jsonify(OP_data)
