17d6f51dacdd85965e7c5c17466263b8a9e2f842
luis.fernando <luiis_engp@hotmail.com>
2026-02-18 14:54:25 -0300

    inserir_usuario_hablitado

--- src/service/OrdemProd_service.py
+++ src/service/OrdemProd_service.py
@@ -118,6 +118,7 @@ class OrdemProd_service():
 
     def inserir_usuario_hablitado(self, codMatricula:str):
         descobrirNome = DashboardTV.DashboardTV('','','','',codMatricula).devolver_nome_usuario()
+        print(descobrirNome)
         nome = descobrirNome[0]
 
         usuario = UsuarioRequisicao.Usuario_requisicao(codMatricula,nome).habilitar_usuario_separacao()
