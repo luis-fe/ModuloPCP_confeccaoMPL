1d622c9c7470e8a2da6238612c00b8ee9bad6484
luis.fernando <luiis_engp@hotmail.com>
2026-03-06 11:23:08 -0300

    api de inserir item a desconsiderar

--- src/routes/Enderecamento_aviamentos.py
+++ src/routes/Enderecamento_aviamentos.py
@@ -195,6 +195,31 @@ def POST_inserir_endereco():
     return jsonify(OP_data)
 
 
+@Enderecamento_routes.route('/pcp/api/inserir_material_desconsiderar_conf', methods=['POST'])
+@token_required
+def POST_inserir_material_desconsiderar_conf():
+    data = request.get_json()
+
+
+    codMaterial = data.get('codMaterial','')
+    codEmpresa = request.args.get('codEmpresa','1')
+
+
+    dados = Enderecamento_aviamentos_service.Enderecamento_aviamento(codEmpresa, '','','','','','',codMaterial).inserirItemDesconsiderar()
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    del dados
+    return jsonify(OP_data)
+
+
 @Enderecamento_routes.route('/pcp/api/conferenciaAviamentos_', methods=['POST'])
 @token_required
 def POST_conferenciaAviamentos_():
