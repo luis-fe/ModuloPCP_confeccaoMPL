973dddfc24edebdae6a6da9f11e00343a69ca312
luis.fernando <luiis_engp@hotmail.com>
2026-03-12 12:33:15 -0300

    atualizando

--- src/configApp/configApp.py
+++ src/configApp/configApp.py
@@ -2,4 +2,4 @@
 '''ARQUIVO DE CONFIGURACAO DE VARIAVEIS GLOBAIS DO PROJETO PYTHON'''
 
 localProjeto = ""
-localArquivoParquet = "/home/grupompl/ModuloPCP_confeccaoMPL/dados"
+localArquivoParquet = ""
