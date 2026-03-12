38b129538094cb4af3b9445933b4ea800ee218b2
luis.fernando <luiis_engp@hotmail.com>
2026-03-12 11:12:58 -0300

    atualizando

--- src/connection/ConexaoERP.py
+++ src/connection/ConexaoERP.py
@@ -23,7 +23,7 @@ def ConexaoInternoMPL():
             'com.intersys.jdbc.CacheDriver',
             f'jdbc:Cache://{host}/CONSISTEM',
             {'user': user, 'password': password},
-            'ModuloPCp/src/connection/CacheDB.jar'
+            '/src/connection/CacheDB.jar'
         )
         yield conn
     finally:
