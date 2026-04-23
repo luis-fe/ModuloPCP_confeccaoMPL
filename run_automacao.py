620925eb21ea87480f312062fc4e126ceffd8693
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2026-04-23 17:53:04 -0300

    Atualização da Automacao

--- run_automacao.py
+++ run_automacao.py
@@ -1,12 +1,11 @@
 import os
 import sys
 from datetime import datetime
-
+from src.service import Automacao_Service
 import psutil
 import pytz
 
 from src.models import Componentes_Csw, Tags_apontadas_defeito_Csw, Pedidos_CSW, OrdemProd
-from src.service import Automacao_Service
 
 
 def obterHoraAtual():
@@ -20,35 +19,25 @@ def obterHoraAtual():
 if __name__ == '__main__':
     PID = os.getpid()
     data = obterHoraAtual()
-    print(f'inicio servico automacao - {data} modificado 02_03_2026/2')
+    print(f'inicio servico automacao versao 18.03  - {data}')
     tempo = 60*6*60
     tempo_tags = 60*10
     tempo_tags2 = 60*30
     tempo_realizadofases = 600
 
-    Tags_apontadas_defeito_Csw.Tags_apontada_defeitos('1',tempo_tags, 20).inserindo_informacoes_tag_postgre()
+    #Tags_apontadas_defeito_Csw.Tags_apontada_defeitos('1',tempo_tags, 20).inserindo_informacoes_tag_postgre()
     #Componentes_Csw.Componentes_CSW('1',tempo).inserirComponentesVariaveis()
     #Tags_apontadas_defeito_Csw.Tags_apontada_defeitos('1',tempo_tags2, 20,'Tags Pilotos').get_tags_pilotos_csw()
 
 
-    pedidosCsw = Pedidos_CSW.Pedidos_CSW('1')
-    pedidosCsw.put_automacao()
+    #pedidosCsw = Pedidos_CSW.Pedidos_CSW('1')
+    #pedidosCsw.put_automacao()
 
+
+    Automacao_Service.Automacao().recebimento_aviamentos_CSW()
 
     ordemProd_Csw = OrdemProd.OrdemProd('1','','','',100,int(tempo_realizadofases))
     ordemProd_Csw.realizado_fases_csw()
 
+
     Automacao_Service.Automacao().buscar_informacao_aviamentos_disponiveis_CSW()
-
-    os.system('clear')
-
-
-    # Iniciar nova instância do script após N segundos
-    new_process = f"{sys.executable} {sys.argv[0]}"
-    print(f'gerado o process {new_process}')
-    os.system(f"sleep 120 && {new_process} &")
-    #Encerrando o Registro de controle do PID
-    p = psutil.Process(PID)
-    p.terminate()
-
-
